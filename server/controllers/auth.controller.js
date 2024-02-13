183ae433fc8b07293719d02e320fa674429f9de5
Anjali <anjalikushwaha030804@gmail.com>
2024-02-13 22:52:31 +0530

    connection is established with mongodb

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,6 +2,7 @@ export const login = (req, res) => {
     console.log('login route');
 }
 export const signup = (req, res) => {
+    res.send('signup route');
     console.log('signup route');
 }
 export const logout = (req, res) => {
--- server/server.js
+++ server/server.js
@@ -3,11 +3,15 @@ dotenv.config();
 import express from "express";
 const app = express();
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDb.js";
 
 const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
-    res.send('Welcome');
+    res.send('Welcome'); 
 })
 
 app.use('/api/auth', authRoutes);
-app.listen(PORT, ()=> console.log(`server is listening on http://localhost:${5000}`));
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`server is listening on http://localhost:${PORT}`)
+});
